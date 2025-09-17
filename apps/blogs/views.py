df6fda2802d866eabf0a8edc896e9e0d958f2145
Abdulhodiyev <armiyatv05@gmail.com>
2025-09-17 15:50:09 +0500

    in process

--- apps/blogs/views.py
+++ apps/blogs/views.py
@@ -1,37 +1,47 @@
-from django.shortcuts import render
+from msilib.schema import ListView
 
 
 from apps.blogs.models import PostModel, CategoryModel, TagModel
+from apps.products.models import ProductModel
 
 
-def blogs_list_view(request):
-    posts = PostModel.objects.all()
-    categories = CategoryModel.objects.all()
-    tags = TagModel.objects.all()
+class BlogListView(ListView):
+    template_name = 'blogs/blog-list-sidebar-left.html'
 
-    context = {
-        'posts': posts,
-        'categories': categories,
-        'tags': tags
-    }
-    return render(request, 'blogs/blog-list-sidebar-left.html', context)
+    def get_get_context_data(self, *args, **kwargs):
+        context =super().get_get_context_data(*args, **kwargs)
+        posts = PostModel.objects.all()
+        categories = CategoryModel.objects.all()
+        tags = TagModel.objects.all()
+
+        context["blogs"] = self.get_queryset()
+        context["posts"] = posts
+        context["categories"] = categories
+        context["tags"] = tags
+
+        return context
 
 
-def blogs_detail_view(request, pk):
-    try:
-        post = PostModel.objects.get(id=pk)
-    except PostModel.DoesNotExist:
-        return render(request, 'pages/404.html')
+class BlogDetailView(ListView):
+    template_name = 'blogs/blog-detail.html'
+    queryset = BlogListView.objects.all()
+    context_object_name = 'blog'
+    pk_url_kwarg = 'pk'
 
-    categories = CategoryModel.objects.all()
-    tags = TagModel.objects.all()
-    related_posts = PostModel.objects.filter(
-        tags__in=post.tags.all()
-    ).exclude(id=post.id).distinct()
-    context = {
-        'post': post,
-        'categories': categories,
-        'tags': tags,
-        'related_posts': related_posts,
-    }
-    return render(request, 'blogs/blog-detail.html', context)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        blog = self.get_object()
+
+        categories = CategoryModel.objects.all()
+        tags = TagModel.objects.all()
+        related_posts = PostModel.objects.filter(
+            tags__in=post.tags.all()
+        ).exclude(id=post.id).distinct()
+
+        context["blogs"] = self.get_queryset()
+        context["categories"] = categories
+        context["tags"] = tags
+        context["related_posts"] = related_posts
+
+        return context
